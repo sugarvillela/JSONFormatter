aa3fd8173871a37152abbab59a8c322de424915c
Dave Swanson <davesswanson@hotmail.com>
2025-05-24 21:45:36 -0700

    Update main.js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,142 +1,618 @@
-const textUtil = new function(){
-    this.isOpener = function(char){
-        return char === '{' || char === '[';
-    };
-    this.isCloser = function(char){
-        return char === '}' || char === ']';
-    };
-    this.isDelimiter = function(char){
-        switch(char){
+const AppState = (() => {
+	const currTokens = [];	// token objects
+	const state = [];		// strings
+	let errState = false;
+	let fixedEqualSign = false;
+	let hideWindow = false;
+	let pointer = 0;
+	
+	const haveState = () => state.length > 0;
+	
+	const dispState = () => {
+		document.getElementById("stateNum").innerHTML = `${pointer+1}/${state.length}`;
+	}
+	const incPointer = () => {
+		pointer = Math.min(pointer + 1, state.length - 1);
+		dispState();
+	}
+	const decPointer = () => {
+		pointer = Math.max(pointer - 1, 0);
+		dispState();
+	}
+	const push = (text) => { 
+		state.push(text);
+		pointer = state.length - 1;
+		dispState();
+	}
+	const back = () => { 
+		decPointer();
+		return state[pointer];
+	}
+	const forward = () => { 
+		incPointer();
+		return state[pointer];
+	}
+	const top = () => { 
+		pointer = state.length - 1;
+		dispState();
+		return state[pointer];
+	}
+	const pop = () => {
+		state.pop();
+		if(state.length){
+			return top();
+		}
+		else {
+			return "";
+		}
+	}
+	const setErr = (newState) => errState = newState;
+	const setFixedEqualSign = (newState) => errState = newState;
+	const isErr = () => errState;
+	
+	toggleHide = () => {
+		hideWindow = !hideWindow;
+		
+		document.getElementById("buttonHide").value = (hideWindow)? "+":"-";
+		document.getElementById("textIn").style.maxHeight = (hideWindow)? "20px":"500px";
+	}
+	
+	return {
+		haveState: () => haveState(),
+		push: (text) => push(text),
+		back: () => back(),
+		forward: () => forward(),
+		top: () => top(),
+		pop: () => pop(),
+		setErr: (newState) => errState = newState,
+		isErr: () => errState,
+		setFixedEqualSign: (newState) => fixedEqualSign = newState,
+		fixedEqualSign: () => fixedEqualSign,
+		toggleHide: () => toggleHide()
+	};
+})();
+
+const DomProxy = (() => {
+	const appendText = (domParent, token, isEndline) => {
+		let domChild = document.createElement("DIV");
+		
+		if(token.isErr) {
+			domChild.classList.add("err");
+		}
+		else {
+			domChild.classList.add(token.className);
+		}
+		
+		domChild.addEventListener("click", token.onClick);
+		token.domObject = domChild;
+		
+		if(token.checkEndline && isEndline){
+			domParent.appendChild(document.createElement("BR"));
+			domChild.innerHTML = FormatUtil.tabHtml(token.indent) + token.value;
+			
+			token.formatted = "\n" + FormatUtil.tabSpace(token.indent) + token.value;
+		}
+		else{
+			domChild.innerHTML = token.value;
+			
+			token.formatted = token.value;
+		}
+		domParent.appendChild(domChild);
+	};
+	const toClipboard = (text) => {
+		let temp = document.createElement("textarea");
+		document.body.appendChild(temp);
+		temp.value = text;
+		temp.select();
+		document.execCommand("copy");
+		document.body.removeChild(temp);
+		document.getElementById('textIn').focus();
+	};
+	
+	return {
+		appendText: (domParent, token, isEndline) => appendText(domParent, token, isEndline),
+		toClipboard: (text) => toClipboard(text)
+	}
+})();
+
+const TokenFactory = (() => {
+	let uq = 1;
+	
+	const getUq = () => {
+		return ++uq;
+	};
+	const getProto = (setValue, linkFunction) => {
+		let id = getUq();
+		
+		return {
+			className : "charDelim",// CSS class for token color
+			id : id,				// id for highlighting on click
+			isOpener : false,		// true for {[
+			isCloser : false,		// true for }]
+			isValue : false,		// true for any non-key text, number or boolean
+			checkEndline : false,	// true if token can be first in the current line (DomProxy adds newLine if needed)
+			isEndline : false,		// true if token can be last in the current line (drawFormatted() passes request to next token)
+			isColon : false,		// true for :
+			isErr: false,			// to be set by errChecker
+			indent : 0,				// set by setIndents()
+			counterpart : undefined,// openers reference closers and vice versa; other tokens leave this undefined 
+			domObject : undefined,	// a reference to the dom object so it can be modified from the AppState.currTokens list
+			value : setValue,		// text content
+			formatted : "",			// DomProxy puts a non-html copy of the formatting here for clipboard
+			onClick :  () => {// Calls a handler either to highlight tokens or clear all
+				linkFunction(id);
+			}
+		};
+	};
+	const getTokenOpen = (setValue) => {// for {[
+		return {
+			...getProto(setValue, FormatUtil.linkView),
+			isOpener: true,
+			checkEndline: true,
+			isEndline: true,
+		};
+	};
+	const getTokenClose = (setValue) => {// for }]
+		return {
+			...getProto(setValue, FormatUtil.linkView),
+			isCloser: true,
+			checkEndline: true,
+			isEndline: true,
+		};
+	};
+	const getComma = () => {
+		return {
+			...getProto(",", FormatUtil.unlinkView),
+			isEndline: true,
+		};
+	};
+	const getColon = () => {
+		return {
+			...getProto(":", FormatUtil.unlinkView),
+			isColon: true,
+		};
+	};
+	const getTokenKey = (setValue) => {
+		return {
+			...getProto(setValue, FormatUtil.unlinkView),
+			className: "charKey",
+			checkEndline: true,
+		};
+	};
+	const getTokenValue = (setValue) => {
+		return {
+			...getProto(setValue, FormatUtil.unlinkView),
+			className: "charValue",
+			isValue: true,
+			checkEndline: true,
+			isEndline: true,
+		};
+	};
+	const getTokenNumValue = (setValue) => {
+		return {
+			...getProto(Number(setValue), FormatUtil.unlinkView),
+			className: "charNumValue",
+			isValue: true,
+			checkEndline: true,
+			isEndline: true,
+		};
+	};
+	const getTokenBoolValue = (setValue) => {
+		const value = (setValue === 'true')? true:false;
+		return {
+			...getProto(value, FormatUtil.unlinkView),
+			className: "charBoolValue",
+			isValue: true,
+			checkEndline: true,
+			isEndline: true,
+		};
+	};
+	
+	return {
+		getTokenOpen: (setValue) => getTokenOpen(setValue),
+		getTokenClose: (setValue) => getTokenClose(setValue),
+		getComma: () => getComma(),
+		getColon: () => getColon(),
+		getTokenKey: (setValue) => getTokenKey(setValue),
+		getTokenValue: (setValue) => getTokenValue(setValue),
+		getTokenNumValue: (setValue) => getTokenNumValue(setValue),
+		getTokenBoolValue: (setValue) => getTokenBoolValue(setValue)
+	};
+})();
+
+const TextUtil = (() => {
+    const readCharacter = (character, id) => {
+        switch(character){
             case '{':
-            case '}':
             case '[':
+				return TokenFactory.getTokenOpen(character, id);
+			case '}':
             case ']':
+				return TokenFactory.getTokenClose(character, id);
             case ',':
-                return true;
+                return TokenFactory.getComma();
+            case ':':
+                return TokenFactory.getColon();
             default:
                 return false;
         }
     };
-    this.isSkip = function(char){
-        return char === '"' || char === "'";
+	const readKeyValToToken = (text, delimToken) => {
+		return (delimToken && delimToken.isColon)? readKeyToToken(text) : readValueToToken(text);
+	};
+	const readKeyToToken = (text) => {
+		return (isQuoted(text))? TokenFactory.getTokenKey(text) : TokenFactory.getTokenKey('"' + text + '"');
+	};
+	const readValueToToken = (text) => {
+		if(isQuoted(text)){
+			return TokenFactory.getTokenValue(text);
+		}
+		else if(isNumeric(text)){
+			return TokenFactory.getTokenNumValue(text);
+		}
+		else if(isBoolean(text)){
+			return TokenFactory.getTokenBoolValue(text);
+		}
+		else {
+			return TokenFactory.getTokenValue('"' + text + '"');
+		}
+	};
+    const isQuoted = (text) => {
+        return text.length > 1 && text[0] === '"' || text[text.length - 1] === "'";
     };
-    this.isEscape = function(char){
-        return char === '\\';
+    const isNumeric = (text) => {
+        return !isNaN(text) && !isNaN(parseFloat(text));
     };
-    this.tab = function(indent){
-        return (indent > 0)? ' '.repeat(indent*4) : "";
+    const isBoolean = (text) => {
+        return text === "true" || text === "false";
     };
-};
-
-const tokenize = (text) => {
-    let tokens = [];
-    let skip = false;
-    let escape = 0;
-    let j = 0;
-    for (var i = 0; i < text.length; i++) {
-        if(textUtil.isEscape(text[i])){
-            escape = 2;
-        }
-        if(0 >= escape--){
-            if(textUtil.isSkip(text[i])){
-                skip = !skip;
-            }
-            else if(!skip && textUtil.isDelimiter(text[i])){
-                if(j !== i){
-                    tokens.push({'t':[text.substring(j, i)], 'ind':0});
-                }
-                tokens.push({'t':[text.substring(i, i + 1)], 'ind':0});
-                j = i + 1;
-            }
-        }
-    }
-    if(j !== i){
-        tokens.push({'t':[text.substring(j, i)], 'ind':0});
-    }
-    return tokens;
-};
-const setIndents = (tokens) => {
-    let indent = 0;
-    for (var i = 0; i < tokens.length; i++){
-        let curr = tokens[i];
-        if(textUtil.isOpener(curr.t[0])){
-            tokens[i].ind = indent;
-            indent++;
-        }
-        else if(textUtil.isCloser(curr.t[0])){
-            indent--;
-            tokens[i].ind = indent;
-        }
-        else{
-            tokens[i].ind = indent;
-        }
-    }
-    return tokens;
-};
-const merge = (tokens) => {
-    let newTokens = [];
-    let prev = tokens[0];
-	let j = 0;
-    newTokens[j++] = prev;
-    for (var i = 1; i < tokens.length; i++){
-        let curr = tokens[i];
-        if(curr === undefined || curr.t[0].trim().length === 0){
-            continue;
-        }
-        if(curr.t[0] === ','){
-            prev.t.push(curr.t[0]);
-        }
-		else if(
-			curr.t[0] === '{' && 
-			!textUtil.isDelimiter(prev.t[prev.t.length - 1])
-		){
-            prev.t.push(curr.t[0]);
-        }
-        else{
-            newTokens[j++] = curr;
-        }
-		prev = curr;
-    }
-    return newTokens;
-};
-const buildStrings = (tokens) => {
-    for (var i = 0; i < tokens.length; i++){
-        let curr = tokens[i];
-        tokens[i] = textUtil.tab(curr.ind) + curr.t.join("");
-    }
-    return tokens;
-};
-const handleText = () => {
-    let text = document.getElementById('textIn').value;
-    text = text.replaceAll(/\r?\n|\r/g, "");
-	document.getElementById("escapeOut").value = escapeText(text);
+    const isQuotedJson = (text) => {
+        return text.length > 3 && text[0] === '"' || text[text.length - 1] === "'" && 
+			((text[1] === '{' || text[text.length - 2] === "}") || (text[1] === '[' || text[text.length - 2] === "]"));
+    };
+    const clearQuotedJson = (text) => {
+        return (isQuotedJson(text))? text.substring(1, text.length - 1) : text;
+    };
+	const removeChars = (text) => {
+		const charList = [' ','\t','\n','\r'];
+		const dest = [];
+		let skip = false;
+		
+		for(let i = 0; i < text.length; i++) {
+			if(text[i] === '"'){
+				skip = !skip;
+				dest.push(text[i]);
+			}
+			else if(skip || !charList.includes(text[i])){
+				dest.push(text[i]);
+			}
+		}
+		return (dest.length === text.length)? text: dest.join("");
+	};
+	const fixBadEqualSign = (text) => {// for fixJavaNotation
+		const charList = [' ','\t','\n','\r'];
+		const dest = [];
+		let skip = false;
+		let changed = false;
+		
+		for(let i = 0; i < text.length; i++) {
+			if(text[i] === '"'){
+				skip = !skip;
+				dest.push(text[i]);
+			}
+			else if(text[i] !== "=" || skip){
+				dest.push(text[i]);
+			}
+			else{
+				dest.push(":");
+				AppState.setFixedEqualSign(true);
+			}
+		}
+		return (AppState.fixedEqualSign())? dest.join(""): text;
+	};
 	
-    let tokens = tokenize(text);
-    if(tokens.length){
-        tokens = buildStrings(
-			merge(
-				setIndents(tokens)
-			)
-		);
-    }
-    document.getElementById("textOut").value = tokens.join('\n');
-};
-const escapeText = (text) => {
-	return text.replaceAll(/["]/g, '\\"')
-}
+	return {
+		readCharacter: (character, id) => readCharacter(character, id),
+		readKeyValToToken: (text, delimToken) => readKeyValToToken(text, delimToken),
+		readKeyToToken: (text) => readKeyToToken(text),
+		readValueToToken: (text) => readValueToToken(text),
+		isQuoted: (text) => isQuoted(text),
+		isNumeric: (text) => isNumeric(text),
+		isBoolean: (text) => isBoolean(text),
+		isQuotedJson: (text) => isQuotedJson(text),
+		clearQuotedJson: (text) => clearQuotedJson(text),
+		removeChars: (text) => removeChars(text),
+		fixBadEqualSign: (text) => fixBadEqualSign(text)
+	};
+})();
+
+const FormatUtil = (() => {
+	const TAB_WIDTH = 4;
+	
+    const tabHtml = (indent) => {
+        return (indent > 0)? '&nbsp;'.repeat(indent*TAB_WIDTH) : "";
+    };
+    const tabSpace = (indent) => {
+        return (indent > 0)? ' '.repeat(indent*TAB_WIDTH) : "";
+    };
+	const setIndents = (tokens) => {
+		let indent = 0;
+		
+		for (var i = 0; i < tokens.length; i++){
+			if(tokens[i].isOpener){
+				tokens[i].indent = indent;
+				indent++;
+			}
+			else if(tokens[i].isCloser){
+				indent--;
+				tokens[i].indent = indent;
+			}
+			else{
+				tokens[i].indent = indent;
+			}
+		}
+	};
+	const isValidCounterpart = (opener, closer) => {
+		switch(opener){
+			case '{':
+				return closer === '}';
+			case '[':
+				return closer === ']';
+			default:
+				return false;
+		}
+	};
+	const setCounterparts = (tokens) => {
+		const stack = [];
+		
+		for (var i = 0; i < tokens.length; i++){
+			if(tokens[i].isOpener){
+				stack.push(tokens[i]);
+			}
+			else if(tokens[i].isCloser){
+				const closer = tokens[i];
+				
+				if(stack.length){
+					const opener = stack.pop();
+					
+					if(isValidCounterpart(opener.value, closer.value)){
+						opener.counterpart = closer;
+						closer.counterpart = opener;
+					}
+					else{
+						opener.isErr = true;
+						closer.isErr = true;
+						AppState.setErr(true);
+					}
+				}
+				else{
+					closer.isErr = true;
+					AppState.setErr(true);
+				}
+			}
+		}
+	};
+	const drawFormatted = (tokens) => {
+		let isEndline = false;
+		let div = document.getElementById("formatOut");
+		div.innerHTML = "";
+		
+		for (var i = 0; i < tokens.length; i++){
+			DomProxy.appendText(div, tokens[i], isEndline);
+			isEndline = tokens[i].isEndline;
+		}
+	};
+	const linkView = (id) => {
+		if(!AppState.isErr()){
+			unlinkView();
+			for(const token of AppState.currTokens){
+				if(token.id == id){
+					token.domObject.setAttribute("class", "");
+					token.domObject.classList.add("highlight");
+					if(token.counterpart){
+						token.counterpart.domObject.setAttribute("class", "");
+						token.counterpart.domObject.classList.add("highlight");
+					}
+				}
+			}
+		}
+	};
+	const unlinkView = () => {
+		for(const token of AppState.currTokens){
+			if(token.id > 0){
+				token.domObject.setAttribute("class", "");
+				token.domObject.classList.add(token.className);
+			}
+		}
+	};
+	
+	return {
+		tabHtml: (indent) => tabHtml(indent),
+		tabSpace: (indent) => tabSpace(indent),
+		setIndents: (tokens) => setIndents(tokens),
+		setCounterparts: (tokens) => setCounterparts(tokens),
+		drawFormatted: (tokens) => drawFormatted(tokens),
+		linkView: (id) => linkView(id),
+		unlinkView: () => unlinkView()
+	}
+})();
+
+const Parser = (() => {
+	const tokenize = (text) => {
+		let tokens = [];
+		let skip = false;
+
+		let j = 0;
+		for (var i = 0; i < text.length; i++) {
+			if(text[i] === '"'){
+				skip = !skip;
+			}
+			else if(!skip) {
+				let delimToken = TextUtil.readCharacter(text[i]);
+				
+				if(delimToken !== false){
+					if(j !== i){
+						tokens.push(
+							TextUtil.readKeyValToToken(text.substring(j, i), 
+							delimToken)
+						);
+					}
+					tokens.push(delimToken);
+					
+					j = i + 1;
+				}
+			}
+		}
+		if(j !== i){
+			tokens.push(
+				TextUtil.readKeyValToToken(text.substring(j, i), 
+				false)
+			);
+		}
+		return tokens;
+	};
+	const fixJavaNotation = (tokens) => {
+		if(AppState.fixedEqualSign()){
+			const remove = [];
+			
+			for(let i = 1; i < tokens.length; i++){
+				const curr = tokens[i];
+				
+				if(curr.isOpener){
+					const prev = tokens[i - 1];
+					if(prev.isValue){
+						remove.push(i - 1)
+					}
+				}
+			}
+			
+			if(remove.length){
+				return tokens.filter((t, i) => !remove.includes(i));
+			}
+		}
+		return tokens;
+	}
+	const handleText = () => {
+		AppState.setErr(false);
+		AppState.setFixedEqualSign(false);
+		
+		let origText = document.getElementById('textIn').value;
+		
+		if(!origText.length){
+			return;
+		}
+		
+		let text = origText.replaceAll(/[\\]["]/g, '"');
+		text = TextUtil.clearQuotedJson(text);
+		text = TextUtil.removeChars(text);
+		text = TextUtil.fixBadEqualSign(text);
+		
+		if(text === origText){
+			let tokens = tokenize(text);
+			tokens = fixJavaNotation(tokens);
+			FormatUtil.setIndents(tokens);
+			FormatUtil.setCounterparts(tokens);
+			FormatUtil.drawFormatted(tokens);
+			AppState.currTokens = tokens;
+		}
+		else{
+			document.getElementById('textIn').value = text;
+		}
+	};
+	
+	return {
+		handleText: () => handleText()
+	};
+})();
+
+// event handlers
 const clearFields = () => {
 	document.getElementById("textIn").value = "";
-	document.getElementById("escapeOut").value = "";
-	document.getElementById("textOut").value = "";
+	document.getElementById("formatOut").innerHTML = "";
 }
+
+const stateSave = () => {
+	let text = document.getElementById('textIn').value;
+	if(text.length){
+		AppState.push(text);
+	}
+}
+
+const stateBack = () => {
+	if(AppState.haveState()){
+		document.getElementById('textIn').value = AppState.back();
+		Parser.handleText();
+	}
+}
+
+const stateForward = () => {
+	if(AppState.haveState()){
+		document.getElementById('textIn').value = AppState.forward();
+		Parser.handleText();
+	}	
+}
+
+const stateTop = () => {
+	if(AppState.haveState()){
+		document.getElementById('textIn').value = AppState.top();
+		Parser.handleText();		
+	}	
+}
+
+const statePop = () => {
+	if(AppState.haveState()){
+		document.getElementById('textIn').value = AppState.pop();
+		Parser.handleText();		
+	}	
+}
+
+const origToClipboard = () => {
+	document.getElementById('textIn').select();
+	document.execCommand("copy");
+}
+
+const escapedToClipboard = () => {
+	let text = document.getElementById('textIn').value;
+	text = text.replaceAll(/["]/g, '\\"');
+	DomProxy.toClipboard(text);
+}
+
+const formattedToClipboard = () => {
+	let formatted = [];
+	for(const token of AppState.currTokens){
+		formatted.push(token.formatted);
+	}
+	DomProxy.toClipboard(formatted.join(""));
+}
+
+
 /*
 Test Data Short:
-
 {"fields":[1,2,3],"thing1":{"innerThing":23},"thing2":"mopey"}
 
 Test Data Long:
-
 {"menu": {"id": "file","value": "File","popup": {"menuitem": [{"value": "New", 
 "onclick": "CreateNewDoc()"},{"value": "Open", "onclick": "OpenDoc()"},{"value": 
 "Close", "onclick": "CloseDoc()"} ]}}}
+
+Escaped
+{\"fields\":[1,2,3],\"thing1\":{\"innerThing\":23},\"thing2\":\"mopey\"}
+
+
+Tab and new line (Due to HTML, multi-space only shows up when format copied to clipboard)
+{a:		b,c    :d 
+, e: "in	quotes 		tabs"}
+
+
+Unnecessary quotes
+"{"fields":[1,2,3],"thing1":{"innerThing":23},"thing2":"mopey"}"
+
+Booleans and Numbers
+{thing:{"id":"file","value":"File","popup":{"menuitem":[{"value":false,"onclick":"doIt()"},{"value":-00033.60000,"onclick":"OpenDoc()"},{"value":true,"onclick":"CloseDoc()"}]}}}
+
+Java map and toString() notation
+[objectName{key1=value1, key2=value2, key3="keep the = sign"}]
+
+Ignore map-like mistake
+[objectName{key1:value1, key2:value2, key3="keep the = sign"}]
  */
